839a02aabbe96ae7dfba35a2f5d37fa948d7c79d
ArnoldLee2015 <arnold.lee.lp@gmail.com>
2025-12-30 15:46:08 +0800

    add jarvis

--- glmtts_inference.sh
+++ glmtts_inference.sh
@@ -9,7 +9,7 @@ get_idle_gpu() {
 
 # Configuration
 exp_name="_test"
-data_list=("jarvis_zh")
+data_list=("jarvis_zh1" "jarvis_zh2" "jarvis_zh3" "jarvis_zh4" "jarvis_zh5")
 sample_rate=24000 # 24000 or 32000
 args="" # "--use_phoneme" or ""
 
